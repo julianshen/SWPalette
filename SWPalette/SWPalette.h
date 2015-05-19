001305c5933d6ffd3f9111f80e1bcc239262b83d
Julian Shen <julianshen@gmail.com>
2015-05-19 17:29:04 +0800

    Initial Commit

---
+++ SWPalette/SWPalette.h
@@ -0,0 +1,19 @@
+//
+//  SWPalette.h
+//  SWPalette
+//
+//  Created by Julian Shen on 2015/5/19.
+//  Copyright (c) 2015年 cowbay.wtf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWPalette.
+FOUNDATION_EXPORT double SWPaletteVersionNumber;
+
+//! Project version string for SWPalette.
+FOUNDATION_EXPORT const unsigned char SWPaletteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWPalette/PublicHeader.h>
+
+
---
+++ SWPaletteTests/SWPaletteTests.swift
@@ -0,0 +1,36 @@
+//
+//  SWPaletteTests.swift
+//  SWPaletteTests
+//
+//  Created by Julian Shen on 2015/5/19.
+//  Copyright (c) 2015年 cowbay.wtf. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SWPaletteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
